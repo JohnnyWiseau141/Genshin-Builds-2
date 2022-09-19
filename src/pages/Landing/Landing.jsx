c099379dda23f54b6d6d9dcbc21fdd7d381c1c67
Ketchup <alexandermaniwang99@gmail.com>
2022-09-19 15:51:52 -0400

    fix daisyUI imports

---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Landing() {
+    return (
+        <>
+            <main className='text-center'>
+                <div>Landing Page</div>
+            </main>
+        </>
+    )
+}
